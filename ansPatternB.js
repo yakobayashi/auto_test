77bc80e3f09f7e551419573d13b683990d19305d
yakobayashi <47299544+yakobayashi@users.noreply.github.com>
2025-01-09 14:46:43 +0900

    Create ansPatternB.js

---
+++ ansPatternB.js
@@ -0,0 +1,48 @@
+// お題「cocolym.txt」読み込み
+const fs = require('fs');
+let text = fs.readFileSync("cocolym.txt", 'utf-8');
+const array = Array.from((text).split(/\n/));
+// console.log(array);
+
+// 【問題1】
+// 「cocolym.txt」から重複を削除する処理を記載
+// ※setオブジェクトは使用禁止
+const pattern_B = (array) => {
+    const result = [];
+    const obj = {};
+    for(let num of array){
+      if (!obj[num]){
+        obj[num] = true;
+        result.push(num);
+      }
+    }
+    return result;
+}
+
+const arr = pattern_B(array);
+
+// 重複削除をいったん表示させてる
+console.log('パターンB：' + pattern_B(array).length);
+console.log(pattern_B(array));
+
+// 【問題2】
+// 整形する処理を記載
+const change = (str) => {
+  const first_line = str[0];
+  const second_line = str[1];
+  const third_line = str[2] + str[3] + str[4] + str[5] + str[6];
+  const fourth_line = str[7];
+  const fifth_line = str[8];
+
+  const idx = second_line.indexOf([...second_line].filter(char => char >= 'A' && char <= 'Z'));
+
+  const answer = first_line.split('').reverse().join('')+'\n'
+         + second_line.slice(idx) + second_line.slice(0,idx)+'\n'
+         + third_line.replace(/\s+/g,'')+'\n'
+         + fourth_line.replace(/[^a-zA-Z^]/g, '')+'\n'
+         + fifth_line.replace("A", "C").replace("a", "c")
+ 
+  return answer;
+}
+
+console.log(change(arr));
